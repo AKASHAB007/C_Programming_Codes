7539cc0aa77b371f1cb7dae890f2bae3c417a596
AKASH A B <69691772+AKASHAB007@users.noreply.github.com>
2020-08-17 15:21:59 +0530

    Update 6_8.c

--- Worked_out_problems/6_8.c
+++ Worked_out_problems/6_8.c
@@ -4,8 +4,33 @@
 #include <conio.h>
 #include <math.h>
 
+int GCD(int m, int n);
 int main()
 {
     int num1,num2;
+    
+    printf("Enter two numbers whose GCD is to be found: ");
+    scanf("%d%d",&num1, &num2);
+    
+    printf("\nGCD of %d and %d is %d\n",num1, num2, GCD(num1,num2));
+    getchar();
 }
-//// password for wifi: sc14z5msk0
+
+int GCD(int a,int b)
+{
+    if(b>a)
+        return GCD(b,a);
+    if(b==0)
+        return a;
+    else
+        return GCD(b,a%b);
+}
+
+/* Sample input:
+18 12
+    
+// Sample output:
+Enter two numbers whose GCD is to be found: 18 12
+GCD of 18 and 12 is 6
+
+*/
